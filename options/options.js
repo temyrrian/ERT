a4292339051e7659d8dee41cf5ccecdabd562b81
Sergey Ponomarenko <Sergey@luckyteam.co.uk>
2015-01-24 11:31:02 +0200

    options

--- options/options.js
+++ options/options.js
@@ -1,27 +1,13 @@
 'use strict';
 
-var serviceKeys = [];
 
-function serviceKeysClick() {
-
-};
 
 function init() {
-	serviceKeys.push(document.getElementById('gl'));
-	serviceKeys.push(document.getElementById('yx'));
-	serviceKeys.push(document.getElementById('bg'));
+	
 
-	for (var i = 0; i < serviceKeys.length; i++) {
-		serviceKeys[i].addEventListener('click', serviceKeysClick);
-	}
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-	// nws = localStorage["n_w_s"].split(',');
-	// document.getElementById("buttonSave").addEventListener("click", saveOptions);
-	// document.getElementById("buttonCansel").addEventListener("click", canselOptions);
-	// document.getElementById("enableAll").addEventListener("click", enableDisableAll);
-	// document.getElementById("disableAll").addEventListener("click", enableDisableAll);
 
 	// init();
 });
